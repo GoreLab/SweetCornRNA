e7030cc77ac26629bcd7b2b434170df54abd36b2
ryokei <ryokei@users.noreply.github.com>
2021-02-08 15:41:29 -0500

    Update 4.1-RunRscript.sh

--- code/script_RT/4.1-RunRscript.sh
+++ code/script_RT/4.1-RunRscript.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-nohup R --vanilla --slave < 4.1-Peer_Use25Fact.R --args BLUE lmer > LOGFILE/out_411.txt &
-nohup R --vanilla --slave < 4.1-Peer_Use25Fact.R --args BLUP lmer > LOGFILE/out_412.txt &
+cd /workdir/rt475/Sweetcorn
+nohup R --vanilla --slave < 4.1-Peer_Use25Fact.R --args BLUE lmer > LOGFILE/out_41.txt &
